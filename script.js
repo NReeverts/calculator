2018fa2063e1d326539df710c6bb2333e0b8e0cd
Noah Reeverts <noah.reeverts@gmail.com>
2026-08-23 18:45:01 -0700

    add Listeners and basic math fuctionality

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let num1, num2, operator;
 const display = document.querySelector(".display");
 const numBtns = Array.from(document.querySelectorAll(".number"));
+const operatorBtns = Array.from(document.querySelectorAll(".operator"));
+const clearBtn = document.querySelector(".clear");
+const equalsBtn = document.querySelector(".equals");
+
+let num1 = "",
+  num2 = "",
+  operator = "";
 
 function add(a, b) {
   return a + b;
@@ -21,20 +27,45 @@ function operate(num1, num2, operator) {
   else if (operator == "*") return multiply(num1, num2);
   else if (operator == "/") return divide(num1, num2);
 }
+function updateOperator(e) {
+  operator = e.target.textContent;
+  display.textContent = operator;
+}
 
 function updateNum1(e) {
   num1 += e.target.textContent;
   display.textContent = num1;
 }
 function updateNum2(e) {
-  num2 = +e.target.textContent;
+  num2 += e.target.textContent;
   display.textContent = num2;
-
 }
-
 
 for (btn of numBtns) {
   btn.addEventListener("click", (e) => {
-    updateNum1(e)
-  })
+    if (!operator) {
+      updateNum1(e);
+    }else{
+      updateNum2(e);
+    }
+  });
 }
+
+for (btn of operatorBtns) {
+  btn.addEventListener("click", (e) => {
+    updateOperator(e);
+  });
+}
+
+equalsBtn.addEventListener("click", () => {
+  let result = operate(+num1, +num2, operator);
+  display.textContent = result
+  
+})
+
+clearBtn.addEventListener("click", () => {
+  num1 = ""
+  num2 = ""
+  operator = ""
+  display.textContent = "CLEARED"
+})
